f978ebcdd164c9765293147fdc116205d1a39b1c
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-03-24 14:42:46 -0500

    Added Postgres DB and updated userIdentity
    Added Postgres DB and updated userIdentity

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -41,6 +41,7 @@ namespace TheBlogProject.Models
     public IFormFile Image { get; set; }
 
     //navigation property
+    [Display(Name ="Author")]
     public virtual BlogUser BlogUser { get; set; }
     public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
 
--- Startup.cs
+++ Startup.cs
@@ -43,9 +43,12 @@ namespace TheBlogProject
 
 
       services.AddIdentity<BlogUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+      .AddDefaultUI()
+      .AddDefaultTokenProviders()
       .AddEntityFrameworkStores<ApplicationDbContext>();
-
       services.AddControllersWithViews();
+
+      services.AddRazorPages();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
